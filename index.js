332554d77026d5fdb521bec288cb9d604cdf9e50
Jakub Pomorski <delveller1@gmail.com>
2024-05-04 09:17:58 +0200

    rate limiter added

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ import express from 'express';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import puppeteer from 'puppeteer';
+import { rateLimit } from 'express-rate-limit';
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    limit: 100,
+    message: 'Too many requests from this IP, try again after 15 minutes'
+});
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -9,13 +16,15 @@ const __dirname = dirname(__filename);
 const app = express();
 const port = 3000;
 
+app.use(limiter);
+
 app.get('/puppeteer', (req, res) => {
     res.sendFile(__dirname + '/src/puppeteer.html');
 });
 
 app.get('/puppeteer_pdf', async (req, res) => {
     URL = req.query.url
-    console.log(URL)
+    console.log(`Requested URL: ${URL}`);
 
     console.time('Time to generate PDF');
     try {
