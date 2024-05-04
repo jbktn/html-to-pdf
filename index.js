fe9fe6577f77f372976e0c414f404c7fa444feb1
Jakub Pomorski <delveller1@gmail.com>
2024-05-04 09:11:05 +0200

    backend tweaks

--- index.js
+++ index.js
@@ -7,24 +7,24 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const app = express();
+const port = 3000;
 
 app.get('/puppeteer', (req, res) => {
     res.sendFile(__dirname + '/src/puppeteer.html');
 });
 
 app.get('/puppeteer_pdf', async (req, res) => {
-    console.log("Puppeteer")
-
     URL = req.query.url
     console.log(URL)
 
+    console.time('Time to generate PDF');
     try {
         const browser = await puppeteer.launch();
         const page = await browser.newPage();
         await page.goto(URL, {
             waitUntil: 'networkidle2'
         });
-        page.setViewport({ width: 1920, height: 1080, deviceScaleFactor: 2});
+        page.setViewport({ width: 3840, height: 2160, deviceScaleFactor: 2});
         page.emulateMediaType('screen');
         const pdf = await page.pdf({ 
             path: 'puppeteer.pdf',
@@ -39,12 +39,13 @@ app.get('/puppeteer_pdf', async (req, res) => {
         console.log(error);
         res.send('An error occurred');
     }
+    console.timeEnd('Time to generate PDF');
 });
 
 app.get('/', (req, res) => {
     res.redirect('/puppeteer');
 });
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+app.listen(port, () => {
+    console.log(`Listening at http://localhost:${port}`);
 });
